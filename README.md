979304887d3e2b0e484f3a265a630148d1017445
pseudozach <git@pseudozach.com>
2021-03-31 22:04:36 -0700

    generate/update commands

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # stxoracle
 
-This cli tool helps to create/deploy/update price feeds on Stacks blockchain that can be used as oracles for other contracts.
+This cli tool helps to deploy and update price feeds on Stacks blockchain that can be used as oracles for other contracts.
 
 ## Requirements
 * Node v12.20.2 (npm v6.14.11)
-* stacks cli if you don't have it already
+* stacks cli 
 `npm install -g @stacks/cli`
 
 ## Installation
@@ -15,33 +15,79 @@ This cli tool helps to create/deploy/update price feeds on Stacks blockchain tha
 
 ## Usage
 * Script needs privatekey as input so convert your mnemonic to privatekey if you dont already have it
-`stx get_stacks_wallet_key --backup_phrase "your 24 word seed phrase"`
 
-* You need following inputs to the script
+```stx get_stacks_wallet_key --backup_phrase "your 24 word seed phrase"```
+
+1. Create a new price feed oracle, you need following inputs to the script:
 ```
+# stxoracle generate --help
+stxoracle generate
+
+Deploy a new price feed oracle contract
+
+Options:
+  --help         Show help                                             [boolean]
+  --version      Show version number                                   [boolean]
+  -b, --base     Base ticker (e.g. BTC)                      [string] [required]
+  -c, --convert  Convert ticker (e.g. USD)                   [string] [required]
+  -p, --privkey  Private key that deploys the oracle contract[string] [required]
+  -k, --pubkey   Public key that deploys the oracle contract [string] [required]
+  -n, --network  Network: mainnet or testnet       [string] [default: "testnet"]
+```
+
+* This command will deploy a new price feed oracle contract on testnet based on [priceoracle.clar](https://github.com/pseudozach/stxoracle/blob/main/priceoracle.clar) that's included in the repository. This contract includes ways to:
+  * Read the price
+  * Update the price
+  * Read oracle address 
+  * Update the oracle address that can update the price
+```
+stxoracle generate -b BTC -c USD -p 'yourprivatekey' -k 'yourstxaddress'
+```
+
+2. Update an existing price feed oracle, you need following inputs to the script:
+```
+# stxoracle update --help
+stxoracle update
+
+Check and update an existing price feed oracle contract
+
+Options:
+  --help                           Show help                           [boolean]
+  --version                        Show version number                 [boolean]
   -b, --base                       Base ticker (e.g. BTC)    [string] [required]
   -c, --convert                    Convert ticker (e.g. USD) [string] [required]
-  --ca, --contractaddress          Oracle contract address   [string] [required]
-  --cn, --contractname             Oracle contract name      [string] [required]
-  --cfu, --contractfunctionupdate  Oracle contract function for update
-                                                             [string] [required]
-  --cfr, --contractfunctionread    Oracle contract function for reading
-                                                             [string] [required]
-  -t, --threshold                  Price threshold for updating price on
-                                   contract               [string] [default: 10]
   -p, --privkey                    Private key that controls oracle contract
                                                              [string] [required]
   -k, --pubkey                     Public key that interacts with oracle
-                                   contract                  [string] [required]
+                                   contract
+                 [string] [default: "ST15RGYVK9ACFQWMFFA2TVASDVZH38B4VAV4WF6BJ"]
+  --ca, --contractaddress          Oracle contract address   [string] [required]
+  --cn, --contractname             Oracle contract name      [string] [required]
+  --cfu, --contractfunctionupdate  Oracle contract function for update
+                                              [string] [default: "update-price"]
+  --cfr, --contractfunctionread    Oracle contract function for reading
+                                                 [string] [default: "get-price"]
+  -t, --threshold                  Price threshold for updating price on
+                                   contract               [string] [default: 10]
   -n, --network                    Network: mainnet or testnet
                                                    [string] [default: "testnet"]
 ```
 
-* Once you have all required inputs, run stxoracle command as below. This command will;
-  * Fetch updated price information from cryptocompare API
+* This command will;
+  * Fetch updated price information from [cryptocompare API](https://min-api.cryptocompare.com/documentation)
   * Fetch current BTC/USD price on the specificed contract on Stacks
   * If the price difference is greater than threshold, update the price on the blockchain.
 
 ```
-stxoracle -b BTC -c USD --ca 'ST15RGYVK9ACFQWMFFA2TVASDVZH38B4VAV4WF6BJ' --cn oracle_btcusd --cfr 'get-price' --cfu 'update-price' -t 8 -p 'yourprivatekey'  -n testnet
+stxoracle update -b BTC -c USD --ca 'yourstxaddress' --cn oracle_btcusd -t 8 -p 'yourprivatekey'
 ```
+
+* Note that it makes sense to run this script as a cron job so it can check the price every 30 minutes and keep it up to date. You could accomplish this by:
+```
+# crontab -e
+*/30 * * * * stxoracle update -b BTC -c USD --ca 'yourstxaddress' --cn oracle_btcusd -t 8 -p 'yourprivatekey'
+```
+
+## Explorer
+For a list of price feed oracles that are deployed by this library, please visit [stxoracle list](https://github.com/pseudozach/stxoracle/wiki/stxoracle-list)
+
--- bin/index.js
+++ bin/index.js
@@ -1,26 +1,54 @@
 #!/usr/bin/env node
 
 import yargs from 'yargs'
-import axios from 'axios';
+import axios from 'axios'
+import fs from 'fs'
 
-import { createStacksPrivateKey, makeContractCall, bufferCVFromString, intCV, broadcastTransaction, callReadOnlyFunction } from '@stacks/transactions';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+import { createStacksPrivateKey, makeContractCall, makeContractDeploy, bufferCVFromString, intCV, broadcastTransaction, callReadOnlyFunction } from '@stacks/transactions';
 import { StacksTestnet, StacksMainnet } from '@stacks/network';
 import BN from "bn.js";
 
 const options = yargs
- // .usage("Usage: -b <base ticker symbol> -c <convert ticker symbol> -t <threshold to update price on contract>")
- .option("b", { alias: "base", describe: "Base ticker (e.g. BTC)", type: "string", demandOption: true })
- .option("c", { alias: "convert", describe: "Convert ticker (e.g. USD)", type: "string", demandOption: true })
- .option("ca", { alias: "contractaddress", describe: "Oracle contract address", type: "string", demandOption: true })
- .option("cn", { alias: "contractname", describe: "Oracle contract name", type: "string", demandOption: true })
- .option("cfu", { alias: "contractfunctionupdate", describe: "Oracle contract function for update", type: "string", demandOption: true })
- .option("cfr", { alias: "contractfunctionread", describe: "Oracle contract function for reading", type: "string", demandOption: true })
- .option("t", { alias: "threshold", describe: "Price threshold for updating price on contract", type: "string"})
- .option("p", { alias: "privkey", describe: "Private key that controls oracle contract", type: "string", demandOption: true })
- .option("k", { alias: "pubkey", describe: "Public key that interacts with oracle contract", type: "string", demandOption: true })
- // .option("m", { alias: "mnemonic", describe: "Mnemonic 24-word seed that controls oracle contract", type: "string", demandOption: true })
- .option("n", { alias: "network", describe: "Network: mainnet or testnet", type: "string"})
- .default({ t : 10, n : "testnet"})
+ .command('generate', 'Deploy a new price feed oracle contract', {
+ 	"b": { alias: "base", describe: "Base ticker (e.g. BTC)", type: "string", demandOption: true },
+ 	"c": { alias: "convert", describe: "Convert ticker (e.g. USD)", type: "string", demandOption: true },
+ 	"p": { alias: "privkey", describe: "Private key that deploys the oracle contract", type: "string", demandOption: true },
+ 	"k": { alias: "pubkey", describe: "Public key that deploys the oracle contract", type: "string", demandOption: true },
+ 	"n": { alias: "network", describe: "Network: mainnet or testnet", type: "string"}
+ }, argv => {})
+ .usage("Generate: stxoracle generate -b BTC -c USD -p 'oracleaccountprivatekey' -k 'ST15RGYVK9ACFQWMFFA2TVASDVZH38B4VAV4WF6BJ'")
+
+ .command('update', 'Check and update an existing price feed oracle contract', {
+ 	"b": { alias: "base", describe: "Base ticker (e.g. BTC)", type: "string", demandOption: true },
+ 	"c": { alias: "convert", describe: "Convert ticker (e.g. USD)", type: "string", demandOption: true },
+ 	"p": { alias: "privkey", describe: "Private key that controls oracle contract", type: "string", demandOption: true },
+ 	"k": { alias: "pubkey", describe: "Public key that interacts with oracle contract", type: "string", default: "ST15RGYVK9ACFQWMFFA2TVASDVZH38B4VAV4WF6BJ" },
+ 	"n": { alias: "network", describe: "Network: mainnet or testnet", type: "string"},
+ 	"ca": { alias: "contractaddress", describe: "Oracle contract address", type: "string", demandOption: true },
+ 	"cn": { alias: "contractname", describe: "Oracle contract name", type: "string", demandOption: true },
+ 	"cfu": { alias: "contractfunctionupdate", describe: "Oracle contract function for update", type: "string", default: "update-price" },
+ 	"cfr": { alias: "contractfunctionread", describe: "Oracle contract function for reading", type: "string", default: "get-price" },
+ 	"t": { alias: "threshold", describe: "Price threshold for updating price on contract", type: "string", default: 10}
+ }, argv => {})
+ .usage("Update: stxoracle update -b BTC -c USD -ca 'ST15RGYVK9ACFQWMFFA2TVASDVZH38B4VAV4WF6BJ' --cn oracle_btcusd -p 'oracleaccountprivatekey'")
+
+ // .option("b", { alias: "base", describe: "Base ticker (e.g. BTC)", type: "string", demandOption: true })
+ // .option("c", { alias: "convert", describe: "Convert ticker (e.g. USD)", type: "string", demandOption: true })
+ // .option("ca", { alias: "contractaddress", describe: "Oracle contract address", type: "string", demandOption: true })
+ // .option("cn", { alias: "contractname", describe: "Oracle contract name", type: "string", demandOption: true })
+ // .option("cfu", { alias: "contractfunctionupdate", describe: "Oracle contract function for update", type: "string" })
+ // .option("cfr", { alias: "contractfunctionread", describe: "Oracle contract function for reading", type: "string" })
+ // .option("t", { alias: "threshold", describe: "Price threshold for updating price on contract", type: "string"})
+ // .option("p", { alias: "privkey", describe: "Private key that controls oracle contract", type: "string", demandOption: true })
+ // .option("k", { alias: "pubkey", describe: "Public key that interacts with oracle contract", type: "string" })
+ // // .option("m", { alias: "mnemonic", describe: "Mnemonic 24-word seed that controls oracle contract", type: "string", demandOption: true })
+ // .option("n", { alias: "network", describe: "Network: mainnet or testnet", type: "string"})
+
+ .default({n : "testnet"})
  .argv;
 
 var network = new StacksTestnet();
@@ -28,78 +56,102 @@ if (options.network != "testnet"){
 	network = new StacksMainnet();	
 }
 
-async function readcontract(options){
-	const result = await callReadOnlyFunction(options);
-	return result;
+if(options._[0] == "update") {
+	// pull data
+	// https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD 
+	var url = "https://min-api.cryptocompare.com/data/price?fsym="+ options.base +"&tsyms="+ options.convert 
+	axios.get(url, { headers: { Accept: "application/json" } })
+	 .then(res => {
+	   // parse response
+	   var newfxprice = parseInt(res.data[options.convert]);
+	   console.log("newfxprice: ", newfxprice);
+
+		// check contract to see if diff > threshold
+		const contractAddress = options.contractaddress;
+		const contractName = options.contractname;
+		const functionName = options.contractfunctionread;
+		// const buffer = bufferCVFromString('foo');
+		const senderAddress = options.pubkey;
+		const readoptions = {
+		  contractAddress,
+		  contractName,
+		  functionName,
+		  functionArgs: [],
+		  network,
+		  senderAddress,
+		};
+		// console.log("read options: ", readoptions);
+		// var result = readcontract(readoptions);
+		// console.log("read result: ", result);
+
+		callReadOnlyFunction(readoptions).then(result => {
+			console.log("oracleprice: ", result.value.value.toString());
+			var oracleprice = parseInt(result.value.value.toNumber());
+
+			if( Math.abs(oracleprice - newfxprice) >= options.threshold){
+			   	//update price on oracle contract
+			   	console.log("need to update price on oracle contract: ", Math.abs(oracleprice - newfxprice) , "is greater than or equal to", options.threshold);
+
+				const txOptions = {
+					contractAddress: options.contractaddress,
+					contractName: options.contractname,
+					functionName: options.contractfunctionupdate,
+					functionArgs: [intCV(newfxprice)],
+					senderKey: options.privkey,
+					validateWithAbi: true,
+					network,
+					// postConditions,
+				};
+				// console.log("txOptions: ", txOptions);
+				makeContractCall(txOptions).then(ccresult => {
+					// console.log("ccresult: ", ccresult);
+					broadcastTransaction(ccresult, network).then(txresult => {
+						console.log("txresult: ", txresult);
+					}).catch(txerr => {
+						console.log("txerr: ", txerr);
+					})
+				}).catch(error => {
+					console.log("makeContractCall error: ", error);
+				});	
+			} else {
+				console.log("value within threshold. no need for an update.");
+			}		
+
+		}).catch(error => {
+			console.log("callReadOnlyFunction error: ", error);
+		});
+
+
+	 }).catch(error => {
+	 	console.log("error: ", error);
+	 });
 }
 
-// pull data
-// https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD 
-var url = "https://min-api.cryptocompare.com/data/price?fsym="+ options.base +"&tsyms="+ options.convert 
-axios.get(url, { headers: { Accept: "application/json" } })
- .then(res => {
-   // parse response
-   var newfxprice = parseInt(res.data[options.convert]);
-   console.log("newfxprice: ", newfxprice);
 
-	// check contract to see if diff > threshold
-	const contractAddress = options.contractaddress;
-	const contractName = options.contractname;
-	const functionName = options.contractfunctionread;
-	// const buffer = bufferCVFromString('foo');
-	const senderAddress = options.pubkey;
-	const readoptions = {
-	  contractAddress,
-	  contractName,
-	  functionName,
-	  functionArgs: [],
+if(options._[0] == "generate") {
+	var contractname = "oracle_"+options.base.toLowerCase()+options.convert.toLowerCase();
+	var contractstr = fs.readFileSync(__dirname + '/../priceoracle.clar').toString();
+	var updatedcontract = contractstr.replace(/ST15RGYVK9ACFQWMFFA2TVASDVZH38B4VAV4WF6BJ/g, options.pubkey);
+
+	const txOptions = {
+	  contractName: contractname,
+	  codeBody: updatedcontract,
+	  senderKey: options.privkey,
 	  network,
-	  senderAddress,
 	};
-	// console.log("read options: ", readoptions);
-	// var result = readcontract(readoptions);
-	// console.log("read result: ", result);
+	// console.log("txOptions: ", txOptions);
 
-	callReadOnlyFunction(readoptions).then(result => {
-		console.log("oracleprice: ", result.value.value.toString());
-		var oracleprice = parseInt(result.value.value.toNumber());
-
-		if( Math.abs(oracleprice - newfxprice) > options.threshold){
-		   	//update price on oracle contract
-		   	console.log("need to update price on oracle contract: ", Math.abs(oracleprice - newfxprice) , "is greater than", options.threshold);
-
-			const txOptions = {
-				contractAddress: options.contractaddress,
-				contractName: options.contractname,
-				functionName: options.contractfunctionupdate,
-				functionArgs: [intCV(newfxprice)],
-				senderKey: options.privkey,
-				validateWithAbi: true,
-				network,
-				// postConditions,
-			};
-			// console.log("txOptions: ", txOptions);
-			makeContractCall(txOptions).then(ccresult => {
-				// console.log("ccresult: ", ccresult);
-				broadcastTransaction(ccresult, network).then(txresult => {
-					console.log("txresult: ", txresult);
-				}).catch(txerr => {
-					console.log("txerr: ", txerr);
-				})
-			}).catch(error => {
-				console.log("makeContractCall error: ", error);
-			});	
-		} else {
-			console.log("value within threshold. no need for an update.");
-		}		
-
-	}).catch(error => {
-		console.log("callReadOnlyFunction error: ", error);
-	});
-
-
- }).catch(error => {
- 	console.log("error: ", error);
- });
+	makeContractDeploy(txOptions).then(transaction => {
+		// console.log("transaction: ", transaction);
+		broadcastTransaction(transaction, network).then(txresult => {
+			console.log("txresult: ", txresult);
+		}).catch(txerr => {
+			console.log("txerr: ", txerr);
+		});
+	}).catch(deployerr => {
+		console.log("deployerr: ",deployerr);
+	})
+	
+}
 
 
